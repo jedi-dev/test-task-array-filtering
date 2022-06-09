166d051c0c82345c7eaa9f1176f9dc092e71d917
jedi-dev <cityman177@gmail.com>
2022-06-09 16:25:40 +0300

    create project and add database

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterComponent } from './filter/filter.component';
+import { ResultComponent } from './result/result.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/mock-items.ts
@@ -0,0 +1,14 @@
+import {ItemInterface} from './item.interface'
+
+export const  items: ItemInterface[] = [
+  {id: 1, name: 'Крепкий орешек', type: 'Боевик'},
+  {id: 2, name: 'Терминатор', type: 'Боевик'},
+  {id: 3, name: 'Унесенные ветром', type: 'Мелодрама'},
+  {id: 4, name: 'Москва слезам не верит', type: 'Мелодрама'},
+  {id: 5, name: 'Оно', type: 'Ужасы'},
+  {id: 6, name: 'Кошмар на улице Вязов', type: 'Ужасы'},
+  {id: 7, name: 'В джазе только девушки', type: 'Комедия'},
+  {id: 8, name: 'Иван Васильевич меняет профессию', type: 'Комедия'},
+  {id: 9, name: 'Аватар', type: 'Фэнтези'},
+  {id: 10, name: 'Гарри Поттер', type: 'Фэнтези'}
+]
